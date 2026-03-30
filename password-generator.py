4ab30da23bfffd8fda3d52ec1c260ac792f98de1
Abdul Rehman <abdulsbaheen@gmail.com>
2026-03-30 11:19:07 +0500

    Enhance password generator with stronger criteria
    Updated password generation logic to include more characters and ensure at least one letter, digit, and symbol in the password.

--- password-generator.py
+++ password-generator.py
@@ -3,16 +3,19 @@ import random,string
 
 print("Welcome to password generator")
 #we are going to create a pool of words for password generation
-letters=''.join(random.choices(string.ascii_letters,k=5))
-digits=''.join(random.choices(string.digits,k=3))
+letters=''.join(random.choices(string.ascii_letters,k=15))
+digits=''.join(random.choices(string.digits,k=9))
 symbols="!@#*&"
+password=""
+password+=random.choice(letters)
+password+=random.choice(digits)
+password+=random.choice(symbols)
 #we are going to combine all the pool of words
 pool_words=letters+symbols+digits
-password=""
 #we are going to ask the user for the length of password
 user_input=int(input("\nEnter the length of password: "))
 #we are going to generate the password by randomly selecting characters from the pool of words
-for i in range (user_input):
+for i in range (user_input-3):
     comp_choice=random.choice(pool_words)
     password+=comp_choice
 print("\nYour strong password is :",password)    
